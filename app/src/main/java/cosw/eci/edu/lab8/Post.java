31399f1f8b85f757376f32e8e2b1b099cf6ddfe1
Hernan Felipe Losada <hernan.losada96@gmail.com>
2018-04-10 19:08:55 -0500

    lab end

---
+++ app/src/main/java/cosw/eci/edu/lab8/Post.java
@@ -0,0 +1,37 @@
+package cosw.eci.edu.lab8;
+
+import android.net.Uri;
+
+import java.io.Serializable;
+
+/**
+ * Created by hernan.losada on 04/10/2018.
+ */
+
+public class Post implements Serializable {
+
+    String message;
+    String imageUri;
+
+    public Post(){}
+
+    public Post(String message,String imageUri){
+        this.message=message;
+        this.imageUri=imageUri;
+    }
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getImageUri() {
+        return imageUri;
+    }
+
+    public void setImageUri(String imageUri) {
+        this.imageUri = imageUri;
+    }
+}
